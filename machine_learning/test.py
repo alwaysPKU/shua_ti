d3ae09fd0ac5dd04f2b519be80541b4985e6333f
alwaysqi <zw_pku@163.com>
2018-05-08 19:02:29 +0800

    find guo

--- machine_learning/test.py
+++ machine_learning/test.py
@@ -1,9 +1,19 @@
-with open('./data_set/data.csv') as f:
-    num = 0
+import json as js
+
+
+dic = dict()
+dic['features_file'] = 'zhangwei_featlst_rdele_labels_0.7'
+dic['labels_file_2'] = 'labels_0.6_0.68_0.7_1.0s'
+dic['labels_file_1'] = 'labels_0.68_dele_0.7'
+dic['parameters_file'] = 'zhangwei_parameters'
+dic['out_put'] = dic['labels_file_2'].split('s_')[1]
+
+# js.dump(dic)
+with open('datavisual_params', 'w') as w:
+    w.write(js.dumps(dic))
+
+with open('datavisual_params') as f:
     line = f.readline()
-    while line:
-        print line
-        num += 1
-        if num == 5:
-            break
-        line = f.readline()
+    duixiang = js.loads(line)
+print line
+
