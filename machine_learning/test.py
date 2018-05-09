6627b17ce19133e058ba92bbd79ece13b0673a3a
alwaysqi <zw_pku@163.com>
2018-05-09 16:50:26 +0800

    new

--- machine_learning/test.py
+++ machine_learning/test.py
@@ -1,19 +1,7 @@
-import json as js
+import numpy as np
+a = [[1,2],[2,3]]
+b = [[1,2]]
+c = [[1,2]]
 
-
-dic = dict()
-dic['features_file'] = 'zhangwei_featlst_rdele_labels_0.7'
-dic['labels_file_2'] = 'labels_0.6_0.68_0.7_1.0s'
-dic['labels_file_1'] = 'labels_0.68_dele_0.7'
-dic['parameters_file'] = 'zhangwei_parameters'
-dic['out_put'] = dic['labels_file_2'].split('s_')[1]
-
-# js.dump(dic)
-with open('datavisual_params', 'w') as w:
-    w.write(js.dumps(dic))
-
-with open('datavisual_params') as f:
-    line = f.readline()
-    duixiang = js.loads(line)
-print line
-
+a =  [v for v in a if v not in b]
+print a
